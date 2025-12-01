197990ec3f2461f760c10905573507408e41a69e
Miguel Diaz <miguelitodiaz169@gmail.com>
2025-11-30 21:02:59 -0500

    add new url for guardians

--- colegio/guardians/urls.py
+++ colegio/guardians/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('board/', AcudienteBoard.as_view(), name='BoardAcudiente'),
+    path('board/', AcudienteBoard.as_view(), name='GuardianBoard'),
+    path('schedule/', AcudienteSchedule.as_view(), name='GuardianSchedule'),
+    path('people/', AcudientePeople.as_view(), name='GuardianPeople'),
+    path('grades/', AcudienteGrades.as_view(), name='GuardianGrades'),
 ]
--- colegio/guardians/views.py
+++ colegio/guardians/views.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView, View
 class AcudienteBoard(View):
     def get(self, request, *args, **kwargs):
         vista = 'guardian'
-        abierto='calendario'
+        abierto='home'
         
         # here we are going to add the guardian's students
         students = request.user.customuserguardian.student.all()
@@ -17,3 +17,48 @@ class AcudienteBoard(View):
         }
         return render(request, 'users/acudiente/inicio.html', context)
 
+
+class AcudienteSchedule(View):
+    def get(self, request, *args, **kwargs):
+        vista = 'guardian'
+        abierto='schedule'
+        
+        # here we are going to add the guardian's students
+        students = request.user.customuserguardian.student.all()
+        
+        context = {
+            'vista': vista,
+            'abierto':abierto,
+            'school': request.user.school,
+        }
+        return render(request, 'users/acudiente/schedule.html', context)
+
+class AcudientePeople(View):
+    def get(self, request, *args, **kwargs):
+        vista = 'guardian'
+        abierto='people'
+        
+        # here we are going to add the guardian's students
+        students = request.user.customuserguardian.student.all()
+        
+        context = {
+            'vista': vista,
+            'abierto':abierto,
+            'school': request.user.school,
+        }
+        return render(request, 'users/acudiente/people.html', context)
+    
+class AcudienteGrades(View):
+    def get(self, request, *args, **kwargs):
+        vista = 'guardian'
+        abierto='grades'
+        
+        # here we are going to add the guardian's students
+        students = request.user.customuserguardian.student.all()
+        
+        context = {
+            'vista': vista,
+            'abierto':abierto,
+            'school': request.user.school,
+        }
+        return render(request, 'users/acudiente/grades.html', context)
